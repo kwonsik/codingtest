834ea29a4293d7b9b55c6dda6929a6981acc1646
kwonsik <155901119+kwonsik@users.noreply.github.com>
2024-03-22 16:33:05 +0900

    [level 0] Title: rny_string, Time: 0.10 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        
+        answer=rny_string.replaceAll("m","rn");
+        
+        return answer;
+    
+    }
+}
