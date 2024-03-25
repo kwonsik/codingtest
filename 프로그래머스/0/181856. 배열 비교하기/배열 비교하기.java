275d2cb85b8e7441c0b5bd35f343654b1d94a4d5
kwonsik <155901119+kwonsik@users.noreply.github.com>
2024-03-25 13:36:28 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        int result1=0;
+        int result2=0;
+        if(arr1.length>arr2.length){answer=1;}
+        else if(arr1.length<arr2.length){answer=-1;}
+        else if(arr1.length==arr2.length){
+            for(int i=0;i<arr1.length;i++){
+                result1+=arr1[i];
+                result2+=arr2[i];
+                if(result1>result2){answer=1;}
+                else if(result1<result2){answer=-1;}
+                else if(result1==result2){answer=0;}
+            }
+        }
+        return answer;
+    }
+}
