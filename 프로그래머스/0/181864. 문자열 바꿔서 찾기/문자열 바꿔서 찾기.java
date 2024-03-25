6af4d29f8022dc67595fa020aacd2cfdfe397966
kwonsik <155901119+kwonsik@users.noreply.github.com>
2024-03-25 17:56:16 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 1.77 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        String result="";
+        for(int i=0;i<myString.length();i++){
+            if(myString.charAt(i)=='A'){result+="B";}
+            else{result+="A";}
+        }
+        if(result.contains(pat)){answer=1;}
+        return answer;
+    }
+}
