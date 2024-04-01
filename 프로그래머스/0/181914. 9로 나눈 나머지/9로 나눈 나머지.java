4067fb6065dd92294a2b5575dcdd05f3851761f3
kwonsik <155901119+kwonsik@users.noreply.github.com>
2024-04-01 16:29:21 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 6.14 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        
+        for(int i=0;i<number.length();i++){
+            String str=""+number.charAt(i);
+            answer+=Integer.parseInt(str);
+            answer=answer%9;
+        }
+        
+        return answer;
+    }
+}
