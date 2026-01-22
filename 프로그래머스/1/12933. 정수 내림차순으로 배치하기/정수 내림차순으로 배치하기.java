03bb365c43eea57044ee451978d9aeb6b576cbcf
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-01-22 16:57:39 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.10 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,24 +1,23 @@
 class Solution {
     public long solution(long n) {
-        long answer = 0;
+        
         int arr[] = new int[10];
         String str = String.valueOf(n);
-        int length = str.length();
-        for(int i=0; i<length; i++) {
-            int temp = str.charAt(i) - 48;
-            arr[temp] += 1;
-            
+
+        for(int i=0; i<str.length(); i++) {
+
+            int temp = str.charAt(i) - '0';
+            arr[temp]++;
         }
-        str = "";
+
+        StringBuilder sb = new StringBuilder();
+        
         for(int i=9; i>=0; i--) {
             for(int j=0; j<arr[i]; j++) {
-                if(arr[i] != 0) {
-                    str += String.valueOf(i);
-                }
-            }        
+                sb.append(i);
+            }
         }
-        answer = Long.parseLong(str);
 
-        return answer;
+        return Long.parseLong(sb.toString());
     }
 }
