b23100bdb0ad0937cf3b2a5955298558f1a2494a
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-01-27 16:16:03 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,13 +1,15 @@
 class Solution {
     public String solution(String phone_number) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<phone_number.length(); i++) {
             if(phone_number.length() - i < 5) {
-                answer += phone_number.charAt(i);
+                sb.append(phone_number.charAt(i));
             } else {
-                answer += "*";
+                sb.append("*");
             }
         }
+        answer = sb.toString();
         return answer;
     }
 }
