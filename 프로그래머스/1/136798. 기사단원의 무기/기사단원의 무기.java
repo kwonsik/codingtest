c6c22690ddd5ebd651d83782c20f864058661ab6
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-02-06 17:04:03 +0900

    [level 1] Title: 기사단원의 무기, Time: 129.46 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,34 +1,28 @@
-import java.util.*;
-
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
-        int cnt = 0;
-        int result = 0;
-        double sqrt = 0;
-        List<Integer> list = new ArrayList<>(); 
-        for(int i=1; i<=number; i++) {
-            cnt = 0;
-            sqrt = Math.sqrt((double)i);
-            for(int j=1; j<=sqrt; j++) {              
-                if(i % j ==0) {
-                    cnt++;
-                    if(j == sqrt) {
-                        result = (cnt)*2 - 1;
-                    } else {
-                        result = cnt*2;
-                    }
-                }               
-            } 
-            list.add(result);
-        }
-        for(int i=0; i<list.size(); i++) {
-            if(list.get(i) > limit) {
+
+        for (int i = 1; i <= number; i++) {
+            int count = 0;
+            
+            // 1. 제곱근까지만 반복 (Math.sqrt 대신 j * j <= i 사용)
+            // 이것이 성능 핵심입니다. (실수 연산 제거)
+            for (int j = 1; j * j <= i; j++) {
+                if (j * j == i) {
+                    count++;        // 제곱수일 경우 (예: 3*3=9) 하나만 카운트
+                } else if (i % j == 0) {
+                    count += 2;     // 약수는 항상 쌍으로 존재 (예: 1, 12 / 2, 6 ...)
+                }
+            }
+
+            // 2. 리스트에 담지 않고 바로 철의 무게 계산 (Limit 체크)
+            if (count > limit) {
                 answer += power;
             } else {
-                answer += list.get(i);
+                answer += count;
             }
         }
+        
         return answer;
     }
 }
