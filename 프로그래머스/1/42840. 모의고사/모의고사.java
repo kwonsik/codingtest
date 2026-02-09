88e992a3f727155ba47f312606f33cbd8a94a38e
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-02-09 17:31:08 +0900

    [level 1] Title: 모의고사, Time: 0.94 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = {2,1,2,3,2,4,2,5};
+        int[] arr3 = {3,3,1,1,2,2,4,4,5,5};
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
+        for(int i=0; i<answers.length; i++) {
+            if(answers[i] == arr1[i % arr1.length]) {
+                cnt1++;
+            }
+            if(answers[i] == arr2[i % arr2.length]) {
+                cnt2++;
+            }
+            if(answers[i] == arr3[i % arr3.length]) {
+                cnt3++;
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        int max = Math.max(cnt1, Math.max(cnt2,cnt3));
+        if(max == cnt1) {
+            list.add(1);
+        }
+        if(max == cnt2) {
+            list.add(2);
+        }
+        if(max == cnt3) {
+            list.add(3);
+        }
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
