e11ac03dc94144c0f9ec7332ed5f82ea0f230bd0
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-02-23 17:04:49 +0900

    [level 1] Title: 문자열 나누기, Time: 0.91 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int index = 0;
+        char ch = ' '; // 첫 글자
+        int cnt1 = 0; // 첫 글자와 같은 게 나온 횟수
+        int cnt2 = 0; // 첫 글자와 다른 게 나온 횟수
+        int length = s.length();
+        for(int i=0;i<length; i++) {
+            ch = s.charAt(index);
+            if(ch == s.charAt(i)) {
+                cnt1++;
+            } else {
+                cnt2++;
+            }     
+            if(cnt1==cnt2) {
+                answer++;
+                index = i+1;
+                cnt1 = 0;
+                cnt2 = 0;
+            }
+            if(cnt1!=cnt2 && i==length-1) {
+                answer++;
+            }
+            
+        }
+        return answer;
+    }
+}
