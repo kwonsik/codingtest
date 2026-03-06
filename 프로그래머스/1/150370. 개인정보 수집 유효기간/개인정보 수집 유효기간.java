266c85f2e9404021858d4cc0461419b41787e9ad
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-03-06 10:51:05 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.58 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies){
+        int[] answer = {};
+        String split[] = new String[2];
+        int year = 0;
+        int month = 0;
+        int date = 0;
+        int differ = 0;
+        Map<String, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<terms.length; i++) {
+            String str1 = terms[i].substring(0,1);
+            String str2 = terms[i].substring(2,terms[i].length());
+            map.put(str1, Integer.parseInt(str2)*28);
+        }
+        System.out.println(map);
+        for(int i=0; i<privacies.length; i++) {
+            // 현재 날짜와 비교
+            split = privacies[i].split(" ");
+            String term = split[1];
+            year = Integer.parseInt(today.substring(0,4)) - Integer.parseInt(split[0].substring(0,4));
+            month = Integer.parseInt(today.substring(5,7)) - Integer.parseInt(split[0].substring(5,7));
+            date = Integer.parseInt(today.substring(8,10)) - Integer.parseInt(split[0].substring(8,10));
+            differ = year*12*28 + month*28 + date;
+            
+            
+            
+            if(differ >= map.get(term)) {
+                list.add(i+1);
+            } 
+            
+        }
+        System.out.println(list);
+        
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
