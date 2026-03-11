beb0abbcc9b9c6af7bc75ae1f683cce06319a48a
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-03-11 15:32:11 +0900

    [level 2] Title: 올바른 괄호, Time: 6.99 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+class Solution {
+    boolean solution(String s) {
+        int count = 0; 
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i); 
+            
+            if (ch == '(') {
+                count++;
+            } else {
+                count--;
+            }
+            
+            if (count < 0) {
+                return false;
+            }
+        }
+        
+        return count == 0;
+    }
+}
