60ec9f18386f32880762971a7b6ec3f4b6f5ebb5
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-03-11 16:56:42 +0900

    [level 2] Title: 숫자의 표현, Time: 0.11 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,23 +1,13 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int sum = 0;
-        for(int i=1; i<=n; i++) {
-            sum = i;
-            if (sum == n) {
+        
+        for (int i = 1; i <= n; i += 2) {
+            if (n % i == 0) {
                 answer++;
-                break;
-            }
-            for(int j=i+1; j<=n; j++) {
-                sum += j;          
-                if(sum == n) {
-                    answer++;
-                    break;
-                } else if(sum>n) {
-                    break;
-                }
             }
         }
+        
         return answer;
     }
 }
