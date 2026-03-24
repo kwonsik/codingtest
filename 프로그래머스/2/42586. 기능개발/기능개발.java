c35f577afd4f938ae48705039d4e7d84189bcdb0
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-03-24 16:33:36 +0900

    [level 2] Title: 기능개발, Time: 0.48 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        boolean finish[] = new boolean[progresses.length];
+        int cnt = 0;
+        int index = 0;
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        while(true) {   
+            cnt++;
+            // 일 진행
+            for(int i=0; i<progresses.length; i++) {
+                progresses[i] += speeds[i];
+                if(progresses[i] >=100) {
+                    finish[i] = true;
+                }
+            }
+            
+            // 배포 가능 체크
+            for(int i=index; i<progresses.length; i++) {
+                if(finish[index]) {
+                    index++;   
+                } else {
+                    break;
+                }
+            }
+            if(stack.size() > 0 && stack.peek() != index) {
+                stack2.push(index - stack.peek());
+            }
+            stack.push(index);
+            
+            if(index == progresses.length) {
+                break;
+            }
+        }
+        answer = new int[stack2.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = stack2.get(i);
+        }
+        return answer;
+    }
+}
