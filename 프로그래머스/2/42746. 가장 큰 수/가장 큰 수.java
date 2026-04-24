7610d5c0658c4fcfe4b7771b6d67b97308c1896f
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-04-24 15:20:14 +0900

    [level 2] Title: 가장 큰 수, Time: 225.20 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strArr = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            strArr[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(strArr, (a, b) -> (b + a).compareTo(a + b));
+        
+        if (strArr[0].equals("0")) {
+            return "0";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (String s : strArr) {
+            sb.append(s);
+        }
+        
+        return sb.toString();
+    }
+}
