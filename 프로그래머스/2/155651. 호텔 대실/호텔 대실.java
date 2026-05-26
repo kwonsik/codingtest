2f4c7336f665b9ecc7b9dcd98199c792b88a868a
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-05-26 17:55:29 +0900

    [level 2] Title: 호텔 대실, Time: 3.49 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -2,34 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
-        // 1. 하루는 24시간 = 1440분. 청소 시간 10분을 더해 넉넉히 1450칸짜리 타임라인 배열 생성
         int[] room = new int[1450];
-        
-        for (String[] time : book_time) {
-            int start = parseTime(time[0]);
-            int end = parseTime(time[1]) + 10;
-
+        int answer = 0;
+        for(int i=0; i<book_time.length; i++) {
+            int start = parseTime(book_time[i][0]);
+            int end = parseTime(book_time[i][1]) + 10;
             room[start] += 1;
-
-            if (end < 1450) {
-                room[end] -= 1; 
+            if(end < 1450) {
+                room[end] -= 1;
             }
         }
-        int answer = 0;
-        int currentRooms = 0; 
-        
-        for (int i = 0; i < 1450; i++) {
-            currentRooms += room[i]; 
-            answer = Math.max(answer, currentRooms);
+        int current = 0;
+        for(int i : room) {
+            current += i;
+            answer = Math.max(answer, current);
         }
-        
         return answer;
     }
     
     private int parseTime(String time) {
-        String[] split = time.split(":");
+        String split[] = time.split(":");
         int hour = Integer.parseInt(split[0]);
-        int minute = Integer.parseInt(split[1]);
-        return hour * 60 + minute;
+        int min = Integer.parseInt(split[1]);
+        return hour * 60 + min;
     }
 }
