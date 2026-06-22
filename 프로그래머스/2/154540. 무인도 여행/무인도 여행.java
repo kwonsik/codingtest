da92c21351540ef6988834dd2a235d53d60f4c8a
kwonsik <155901119+kwonsik@users.noreply.github.com>
2026-06-22 16:39:38 +0900

    [level 2] Title: 무인도 여행, Time: 5.28 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -7,25 +7,31 @@ class Solution {
     int rows, cols; // 클래스 레벨 변수
     
     public int[] solution(String[] maps) {
+        int answer[] = {};
         rows = maps.length;
         cols = maps[0].length();
-        List<Integer> list = new ArrayList<>();
         visited = new boolean[rows][cols];
+        List<Integer> list = new ArrayList<>();
         
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(maps[i].charAt(j) != 'X' && !visited[i][j]) {
+        for(int i=0; i< rows; i++) {
+            for(int j=0; j<cols; j++) {
+                char ch = maps[i].charAt(j);
+                if(ch != 'X' && !visited[i][j]) {
                     int result = bfs(maps, i, j);
-                    list.add(result); 
-                }              
+                    list.add(result);
+                }
             }
         }
-        
-        if(list.isEmpty()) return new int[]{-1};
-        Collections.sort(list);
-        return list.stream().mapToInt(i -> i).toArray();
+        if(list.size() == 0) {
+            return new int[]{-1};
+        }
+        answer = new int[list.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        Arrays.sort(answer);
+        return answer;
     }
-    
     public int bfs(String[] maps, int startRow, int startCol) {
         int result = maps[startRow].charAt(startCol) - '0';
         ArrayDeque<int[]> deque = new ArrayDeque<>();
@@ -33,23 +39,24 @@ class Solution {
         visited[startRow][startCol] = true;
         
         while(!deque.isEmpty()) {
-            int cur[] = deque.poll();
-            int curRow = cur[0];
-            int curCol = cur[1];
-            
-            for(int i = 0; i < 4; i++) {
-                int nextRow = curRow + dy[i];
-                int nextCol = curCol + dx[i]; 
-                
+            int[] current = deque.poll();
+            int currentRow = current[0];
+            int currentCol = current[1];
+            for(int i=0; i<4; i++) {
+                int nextRow = currentRow + dy[i];
+                int nextCol = currentCol + dx[i];
                 if(nextRow >= 0 && nextCol >= 0 && nextRow < rows && nextCol < cols) {
                     if(!visited[nextRow][nextCol] && maps[nextRow].charAt(nextCol) != 'X') {
+                        result += maps[nextRow].charAt(nextCol) - '0';
                         visited[nextRow][nextCol] = true;
                         deque.offer(new int[]{nextRow, nextCol});
-                        result += maps[nextRow].charAt(nextCol) - '0';
                     }
                 }
             }
+            
         }
+        
+        
         return result;
     }
 }
